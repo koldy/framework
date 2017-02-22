8a4bd802cdb508a21fc7f151780c94139c680943
Vlatko Koudela <vlatko@koudela.org>
2017-02-22 11:59:48 +0100

    Ability to define in db config if you want fw to throw exception when record is not found

---
+++ src/Koldy/Db/Exception/NotFoundException.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types = 1);
+
+namespace Koldy\Db\Exception;
+
+/**
+ * Class RecordNotFoundException
+ * @package Koldy\Db\Exception
+ *
+ * Db methods will return null if record wasn't found. If you want to throw new Exception, you may use this one.
+ */
+class NotFoundException extends \Koldy\Db\Exception
+{
+
+}
