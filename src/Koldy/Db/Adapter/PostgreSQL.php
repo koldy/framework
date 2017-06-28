82c8ef781ecbcca07511486ddb42e4eba5f1a3d7
Vlatko Koudela <vlatko@koudela.org>
2017-06-28 13:29:10 +0200

    Removed MySQL attr use buffered query from Postgres adapter

--- src/Koldy/Db/Adapter/PostgreSQL.php
+++ src/Koldy/Db/Adapter/PostgreSQL.php
@@ -50,7 +50,6 @@ class PostgreSQL extends AbstractAdapter
         $pdoConfig = array(
           PDO::ATTR_EMULATE_PREPARES => false,
           PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-          PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => false,
           PDO::ATTR_PERSISTENT => $config['persistent'] ?? true,
           PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         );
