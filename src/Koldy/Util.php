6dea2b5f9347cd3e7101d869b66343cd95501e8a
Vlatko Koudela <vlatko@koudela.org>
2017-11-23 19:58:08 +0100

    Added Util::getRelativePath() method

--- src/Koldy/Util.php
+++ src/Koldy/Util.php
@@ -414,4 +414,46 @@ class Util
         return is_string($str) && preg_match('~[^\x20-\x7E\t\r\n]~', $str) > 0;
     }
 
+    /**
+     * Gets relative path from one absolute path to another
+     *
+     * @param string $from
+     * @param string $to
+     * @return string
+     *
+     * @link https://stackoverflow.com/questions/2637945/getting-relative-path-from-absolute-path-in-php
+     */
+    public static function getRelativePath(string $from, string $to): string
+    {
+        // some compatibility fixes for Windows paths
+        $from = is_dir($from) ? rtrim($from, '\/') . '/' : $from;
+        $to = is_dir($to) ? rtrim($to, '\/') . '/' : $to;
+        $from = str_replace('\\', '/', $from);
+        $to = str_replace('\\', '/', $to);
+
+        $from = explode('/', $from);
+        $to = explode('/', $to);
+        $relPath = $to;
+
+        foreach ($from as $depth => $dir) {
+            // find first non-matching dir
+            if ($dir === $to[$depth]) {
+                // ignore this directory
+                array_shift($relPath);
+            } else {
+                // get number of remaining dirs to $from
+                $remaining = count($from) - $depth;
+                if ($remaining > 1) {
+                    // add traversals up to first matching dir
+                    $padLength = (count($relPath) + $remaining - 1) * -1;
+                    $relPath = array_pad($relPath, $padLength, '..');
+                    break;
+                } else {
+                    $relPath[0] = './' . $relPath[0];
+                }
+            }
+        }
+        return implode('/', $relPath);
+    }
+
 }
