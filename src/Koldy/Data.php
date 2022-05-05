0fb22a7962187999c22f4fe4370be5de6417fe4e
Vlatko Koudela <vlatko@koudela.org>
2022-05-06 00:05:14 +0200

    Backcompatibility fix in Data trait

--- src/Koldy/Data.php
+++ src/Koldy/Data.php
@@ -24,37 +24,45 @@ trait Data
         return $this->data;
     }
 
-    /**
-     * Set data - it'll override all existing data. If you want to "append" data, use addData() method.
-     *
-     * @param array $data
-     */
-    final public function setData(array $data): void
+	/**
+	 * Set data - it'll override all existing data. If you want to "append" data, use addData() method.
+	 *
+	 * @param array $data
+	 *
+	 * @return Data
+	 */
+    final public function setData(array $data): static
     {
         $this->data = $data;
+	    return $this;
     }
 
-    /**
-     * Add/append data to already existing data by using array_merge().
-     *
-     * @param array $data
-     */
-    final public function addData(array $data): void
+	/**
+	 * Add/append data to already existing data by using array_merge().
+	 *
+	 * @param array $data
+	 *
+	 * @return Data
+	 */
+    final public function addData(array $data): static
     {
         $this->data = array_merge($this->data, $data);
+		return $this;
     }
 
-    /**
-     * Set the key into JSON response
-     *
-     * @param string $key
-     * @param mixed $value
-     *
-     * @link http://koldy.net/docs/json#usage
-     */
-    final public function set(string $key, mixed $value): void
+	/**
+	 * Set the key into JSON response
+	 *
+	 * @param string $key
+	 * @param mixed $value
+	 *
+	 * @return Data
+	 * @link http://koldy.net/docs/json#usage
+	 */
+    final public function set(string $key, mixed $value): static
     {
         $this->data[$key] = $value;
+	    return $this;
     }
 
     /**
@@ -70,24 +78,27 @@ trait Data
         return array_key_exists($key, $this->data);
     }
 
-    /**
-     * Remove the JSON key from data
-     *
-     * @param string $key
-     *
-     * @link http://koldy.net/docs/json#usage
-     */
-    final public function delete(string $key): void
+	/**
+	 * Remove the JSON key from data
+	 *
+	 * @param string $key
+	 *
+	 * @return Data
+	 * @link http://koldy.net/docs/json#usage
+	 */
+    final public function delete(string $key): static
     {
         unset($this->data[$key]);
+	    return $this;
     }
 
     /**
      * Remove all data that was stored in this instance
      */
-    final public function deleteAll(): void
+    final public function deleteAll(): static
     {
         $this->data = [];
+	    return $this;
     }
 
     /**
